f212643182d39d3529f64409dc6c833da56ea97e
Heba Mamdooh <52792349+HebaMamdooh@users.noreply.github.com>
2022-08-09 16:03:03 +0200

    add initial packages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom';
-
-import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.rtl.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
