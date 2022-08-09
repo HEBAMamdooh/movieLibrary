bf4a3d03ae1f58a64db8f2238795f5f9f67a02ac
Heba Mamdooh <52792349+HebaMamdooh@users.noreply.github.com>
2022-08-09 18:06:33 +0200

    loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import Home from './components/Home';
+import Loader from './components/Loader/Loader';
+
 function App() {
   return (
     <>
-      <Routes>
-        <Navbar />
-
+        <Loader />
+        <Home />
         <Footer />
-      </Routes>
+        <Navbar />
     </>
   );
 }
