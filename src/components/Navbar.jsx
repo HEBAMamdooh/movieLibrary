88fba9a6c9da9c279800b19a54a39277d9d2bcac
Heba Mamdooh <52792349+HebaMamdooh@users.noreply.github.com>
2022-08-10 18:34:12 +0200

    navbar updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,23 +9,23 @@ export default function Navbar() {
       <nav className='navbar navbar-dark navbar-expand-lg bg-dark'>
         <div className='container-fluid '>
           <NavLink className='navbar-brand' to='/' end><img src={logo} alt='الشعار' /></NavLink>
-          <a className='navbar-brand' href='/'> </a>
           <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarTogglerDemo02' aria-controls='navbarTogglerDemo02' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
           <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
             <ul className='navbar-nav me-auto mb-2 mb-lg-0 '>
               <li className='nav-item '>
-                <a className='nav-link active' aria-current='page' href='/'>المكتبة</a>
+                <NavLink className='nav-link' to='/archive' aria-current='page'>المكتبة</NavLink>
               </li>
               <li className='nav-item'>
-                <a className='nav-link ' href='/'>اضيف حديثا</a>
+                <NavLink className='nav-link' to='/latest'>اضيف حديثا</NavLink>
               </li>
               <li className='nav-item'>
-                <a className='nav-link '>الطلبات</a>
+                <NavLink className='nav-link' to='/requests'>الطلبات</NavLink>
               </li>
             </ul>
             <Search />
+            
           </div>
         </div>
       </nav>
---
+++ src/components/Recent/Recent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Recent() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Recent
